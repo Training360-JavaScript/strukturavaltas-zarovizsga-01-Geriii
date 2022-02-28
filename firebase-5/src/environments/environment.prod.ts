b40d5f0d2e2bf2ab1e48b08173de0c3c967d1548
Geriii <k.geri6@gmail.com>
2022-02-28 16:10:24 +0100

    firebase deployed

---
+++ firebase-5/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAJYaq-lPIRQU-XckkAqNcjuQTMGSH2Ss",
+  authDomain: "webtopus-project.firebaseapp.com",
+  projectId: "webtopus-project",
+  storageBucket: "webtopus-project.appspot.com",
+  messagingSenderId: "1001307968461",
+  appId: "1:1001307968461:web:3bc029bd9e2b5faf152a84",
+  measurementId: "G-YKT4V030E0"
+};
+
+export const environment = {
+  production: true,
+  firebaseConfig
+};
---
+++ firebase-5/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+const firebaseConfig = {
+  apiKey: "AIzaSyAAJYaq-lPIRQU-XckkAqNcjuQTMGSH2Ss",
+  authDomain: "webtopus-project.firebaseapp.com",
+  projectId: "webtopus-project",
+  storageBucket: "webtopus-project.appspot.com",
+  messagingSenderId: "1001307968461",
+  appId: "1:1001307968461:web:3bc029bd9e2b5faf152a84",
+  measurementId: "G-YKT4V030E0"
+};
+
+export const environment = {
+  production: false,
+  firebaseConfig
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
